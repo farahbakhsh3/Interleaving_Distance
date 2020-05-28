efdd580f4831491d38fcc7643c283b3b03633646
Elena Farahbakhsh Touli <elena.touli@math.su.se>
2020-05-28 21:55:44 +0200

    Last Update

--- Main.py
+++ Main.py
@@ -25,3 +25,15 @@ for pre, fill, node in RenderTree(udo):
 
 print(dan.children)
 # (Node('/Udo/Dan/Jet'), Node('/Udo/Dan/Jan'), Node('/Udo/Dan/Joe'))
+
+class MyBaseClass(object):  # Just an example of a base class
+   foo = 4
+class MyClass(MyBaseClass, NodeMixin):  # Add Node feature
+    def __init__(self, name, length, width, parent=None, children=None):
+       super(MyClass, self).__init__()
+       self.name = name
+       self.length = length
+     self.width = width
+         self.parent = parent
+         if children:  # set children only if given
+            self.children = children
